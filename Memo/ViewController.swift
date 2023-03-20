2003d6e80a2fb6541222dd717cf2f9bcd4937a20
jackey6621 <jackey.yjy@gmail.com>
2023-03-20 18:26:03 -0400

    Initial Commit

---
+++ Memo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Memo
+//
+//  Created by Jingyuan Yang on 3/20/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
