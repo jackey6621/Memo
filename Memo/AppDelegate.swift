93abca8ac0e237a305d6e41cdae29987cbda4c65
jackey6621 <jackey.yjy@gmail.com>
2023-03-22 18:14:33 -0400

    login

--- Memo/AppDelegate.swift
+++ Memo/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,11 +15,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let configuration = ParseClientConfiguration {
+            $0.applicationId = "vMcEtS3P7wOzr17Am8gazdB7aYd4IXIYb0nz2bLy"
+            $0.clientKey = "NlN9mwRT0ulbcIwKpVqspJsmtr8aQHgbVGPzNXEM"
+            $0.server = "https://parseapi.back4app.com"
+        }
+        Parse.initialize(with: configuration)
+
         return true
     }
 
     // MARK: UISceneSession Lifecycle
-
+    
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
--- Memo/ViewController.swift
+++ Memo/ViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import ParseSwift
+import Parse
 
 class ViewController: UIViewController {
 
@@ -15,5 +17,56 @@ class ViewController: UIViewController {
     }
 
 
+    @IBOutlet weak var usernameText: UITextField!
+    @IBOutlet weak var passwordText: UITextField!
+    
+    func alert(message: NSString, title: NSString) {
+        let alert = UIAlertController(title: title as String, message: message as String, preferredStyle: UIAlertController.Style.alert)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+
+        }
+    
+    @IBAction func login(_ sender: Any) {
+        // Retrieving the info from the text fields
+               let username = usernameText.text
+               let password = passwordText.text
+               
+               // Defining the user object
+               PFUser.logInWithUsername(inBackground: username!, password: password!, block: {(user, error) -> Void in
+                   if let error = error as NSError? {
+                       let errorString = error.userInfo["error"] as? NSString
+                       self.alert(message: errorString!, title: "Error")
+                   }
+                   else {
+                       self.alert(message: "Welcome back!", title: "Login")
+                   }
+               })
+    }
+    @IBAction func register(_ sender: Any) {
+        // Retrieving the info from the text fields
+                let username = usernameText.text
+                let password = passwordText.text
+                
+                // Defining the user object
+                let user = PFUser()
+                user.username = username
+                user.password = password
+                
+                // We won't set the email for this example;
+                // Just for simplicity
+                
+                // Signing up using the Parse API
+                user.signUpInBackground {
+                    (success, error) -> Void in
+                    if let error = error as NSError? {
+                        let errorString = error.userInfo["error"] as? NSString
+                        self.alert(message: errorString!, title: "Error")
+                        
+                    } else {
+                        self.alert(message: "Registered successfully", title: "Registering")
+                    }
+                }
+    }
 }
 
